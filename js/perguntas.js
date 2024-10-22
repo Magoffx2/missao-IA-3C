d02555f8a560bd1348c2a5a0a86e83701ab94896
Magoffx2 <107898413+Magoffx2@users.noreply.github.com>
2024-10-22 07:49:26 -0300

    .

---
+++ js/perguntas.js
@@ -0,0 +1,119 @@
+export const perguntas = [
+    {
+        enunciado: "Você assistiu o atletico levando uma tava do corinthians ?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "ele vai ser rebaixado "
+                    ],
+                proxima: 1,
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    "mae do tecnico"
+                    ],
+                proxima: 1, 
+            },
+        ]
+    },
+    
+    {
+        enunciado: "qual seleção ganhou mais copa do mundo ?",
+        alternativas: [
+            {
+                texto:"Brasil",
+                afirmacao: [
+                    " ganhou 5 copa do mundo. "
+                    ],
+                proxima: 2, 
+            },
+            {
+                texto: "França",
+                afirmacao: [
+                    " tem nada "
+                    ],
+                proxima: 2, 
+            },
+        ]
+    },
+    
+    {
+        enunciado: "em que ano foi realizado a primeira copa do mundo ?",
+        alternativas: [
+            {
+                texto:"1930",
+                afirmacao: [
+                    "1930"
+                    ],
+                proxima: 3, 
+            },
+            {
+                texto:"1929",
+                afirmacao: [
+                    "1929, "
+                ],
+                proxima: 3, 
+            },
+        ]
+    },
+    
+    {
+        enunciado: "qual jogador que venceu mais bola de ouro ?",
+        alternativas: [
+            {
+                texto: "messi",
+                afirmacao: [
+                    "ganhou 7 vezes"
+                    ],
+                proxima: 4, 
+            },
+            {
+                texto: "Ronaldo",
+                afirmacao: [
+                    "robo da web"
+                ],
+                proxima: 4, 
+            },     
+        ]
+    },
+    
+    {
+        enunciado: "quem é o maior artilheiro da historia da seleçao Brasileira?",
+        alternativas: [
+            {
+                texto: "neymar",
+                afirmacao: [
+                    ".a fera das mulheres "
+                    ],
+                proxima: 5, 
+            },
+            {
+                texto: "Romario",
+                afirmacao: [
+                    ".rei da penha"
+                    ],
+                proxima: 5, 
+            },
+        ]
+    },
+    
+    {
+        enunciado: "quantos títulos da copa america o Brasil ja conquistou ?",
+        alternativas: [
+            {
+                texto: "9",
+                afirmacao: [
+                    " 9 titulos "
+                    ],
+            },
+            {
+                texto: "7",
+                afirmacao: [
+                    " 7 errado"
+                    ],
+            },   
+        ]
+    },
+];
